94741b21d36de3e4822f4e079895244bb3b88c01
yeghia-s <yeghiasargis@yahoo.com>
2026-03-12 22:53:40 -0400

    add terraform provider and test vm config

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -1,1 +1,23 @@
 # VM resources will go here
+resource "proxmox_virtual_environment_vm" "test" {
+  name      = "terraform-test"
+  node_name = "proxmox"
+
+  cpu {
+    cores = 1
+  }
+
+  memory {
+    dedicated = 512
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+    interface    = "virtio0"
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+}
